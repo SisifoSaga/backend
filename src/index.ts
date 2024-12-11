68420c95b64440f635a12668ef8d6fb217cf4818
Cristian Fuentes Rosas <fuentesrosascristian@gmail.com>
2024-12-11 00:51:37 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import colors from 'colors'
 import server from './server'
 
 const port = process.env.PORT || 4000
-
 server.listen(port, () => {
-    console.log( colors.cyan.bold( REST API en el puerto ${port}))
+    console.log( colors.cyan.bold( `REST API en el puerto ${port}`))
 })
