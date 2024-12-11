9f90cbe3c8905115f0065468416383acf594a1d4
Cristian Fuentes Rosas <fuentesrosascristian@gmail.com>
2024-12-10 23:30:20 -0600

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,18 @@
 import { Sequelize } from 'sequelize-typescript'
-import dotenv from 'dotenv'
+import pg from "pg"
 dotenv.config()
 
-const db = new Sequelize(process.env.DATABASE_URL!, {
+const db = new Sequelize('postgresql://rest_api_node_typescript_c5gd_user:nuC2YnHgKnvlhK5AcggicXadx4asADnF@dpg-ctaa3lggph6c73emjah0-a.oregon-postgres.render.com/rest_api_node_typescript_c5gd', {
+    dialect: 'postgres',
+    dialectModule: pg,
     models: [__dirname + '/../models/**/*'],
-    logging: false
+    logging, false,
+    dialectOptions: {
+        ssl:{
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 })
 
 export default db
