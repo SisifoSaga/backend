378670d4bdc40f48171fceadd69f185929213ba6
Cristian Fuentes Rosas <fuentesrosascristian@gmail.com>
2024-12-10 23:34:58 -0600

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,5 @@
 import { Sequelize } from 'sequelize-typescript';
 import pg from "pg"
-dotenv.config()
 
 const db = new Sequelize('postgresql://rest_api_node_typescript_c5gd_user:nuC2YnHgKnvlhK5AcggicXadx4asADnF@dpg-ctaa3lggph6c73emjah0-a.oregon-postgres.render.com/rest_api_node_typescript_c5gd', {
     dialect: 'postgres',
